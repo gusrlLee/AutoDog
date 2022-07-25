a0c69460a2aefd00c9710f9e34d725db60a876b4
gusrlLee <gusrl3204@naver.com>
2022-07-25 13:24:39 +0900

    2022-07-25 start lidar library

---
+++ include/Map.h
@@ -0,0 +1,18 @@
+#ifndef __MAP__
+#define __MAP__
+
+#include <opencv2/opencv.cpp>
+#include <iostream>
+#include <map>
+#include <algorithm>
+#include <vector>
+#include <string>
+
+class Map {
+    public:
+        cv::Mat m_map_image;
+};
+
+
+#endif // __MAP__
+
